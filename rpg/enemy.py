9fe8a9c2d60deb7ce6b07b4b001d2d63f1b056d4
thebrucepro <146689693+thebrucepro@users.noreply.github.com>
2025-07-25 19:31:41 -0500

    Create enemy.py

---
+++ rpg/enemy.py
@@ -0,0 +1,9 @@
+class Enemy:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.max_hp = hp
+
+    def is_alive(self):
+        return self.hp > 0
+      
